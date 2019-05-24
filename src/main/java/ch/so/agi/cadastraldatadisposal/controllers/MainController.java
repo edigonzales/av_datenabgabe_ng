656661fdfd7d23325ab06d0d534e17b4dfa496e8
Stefan Ziegler <edi.gonzales@gmail.com>
2019-05-24 11:40:20 +0200

    switch from jsf to mvc (thymeleaf)

---
+++ src/main/java/ch/so/agi/cadastraldatadisposal/controllers/MainController.java
@@ -0,0 +1,38 @@
+package ch.so.agi.cadastraldatadisposal.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.so.agi.cadastraldatadisposal.models.Dataset;
+import ch.so.agi.cadastraldatadisposal.services.DatasetService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+public class MainController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    DatasetService dataSetService;
+
+    List<Dataset> lstDatasets = new ArrayList<Dataset>();
+
+    @PostConstruct
+    public void init() throws Exception {
+        lstDatasets = dataSetService.getDatasets();
+    }
+    
+    @GetMapping("/")
+    public String getDataset(Model model) {
+        model.addAttribute("datasets", lstDatasets);
+        return "dataset.table.html";
+    }
+}
